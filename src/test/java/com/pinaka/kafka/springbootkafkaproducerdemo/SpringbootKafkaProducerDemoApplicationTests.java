4c2f43b14bc9e818ed57774a4e3a94dbebdbca85
Ravi Tale <ravitale02@gmail.com>
2020-06-14 14:07:22 +0200

    initial commit

---
+++ src/test/java/com/pinaka/kafka/springbootkafkaproducerdemo/SpringbootKafkaProducerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinaka.kafka.springbootkafkaproducerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProducerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
